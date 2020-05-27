2cfc5bf95d05ed7d9312661b0df00e212ad02c99
Marco Ziesing <m.ziesing@mediacluster.de>
2020-05-27 10:53:19 +0200

    [TASK] Allow install in TYPO3 v10.4 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.1.0-7.3.99',
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
